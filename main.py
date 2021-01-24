96a04ca31a6674d43eecc45dbcc92e063937d775
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-24 07:54:01 +0000

    Check the user chances

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 """
 This application is a basic hangman game.
 A random word is chosen from a list and length of the word is showed in blanks to the user.
-A user start's the game with 7 chances to guess the word correctly by guessing the letters in the word.
+A user start's the game with 5 chances to guess the word correctly by guessing the letters in the word.
 If the user guessed correctly before the chances is at 0 they win else they lose,
 while each wrong guess means the user loses a chance.
 """
 # Add the imports
 from words import words
+from logo import chances_stage
 import random
 
 # Choose a random word from the list of words
@@ -19,17 +20,18 @@ print(f'The selected word is: {selected_word}\n')
 print('Game Instructions: \n'
       'A random word is selected and the length of the word is shown in blanks.\n'
       'Guess the word by guessing the letters that are in it.\n'
-      'You start with 7 chances to get the word correctly and win the game.\n')
+      'You start with 5 chances to get the word correctly and win the game.\n')
 
 # Creating a blank the length of the word
 selected_word_length = len(selected_word)
-blanks = []
+user_display = []
 for letter in range(selected_word_length):
-    blanks += '_'
-print(f'Guess the word: {blanks}')
+    user_display += '_'
+    user_display_string = ' '.join(user_display)
+print(f'Guess the word: {user_display_string}')
 
 # Set the user chances
-chances = 7
+chances = 5
 print(f'Chances: {chances}\n')
 
 # Add a while loop to iterate until the user guess correctly
@@ -37,16 +39,33 @@ is_game_over = False
 
 while not is_game_over:
     # Prompt the user
-    user = input('Guess a letter that exists in the word: ').lower()
+    user_guess = input('Guess a letter that exists in the word: ').lower()
 
     # Loop through to check if the user input exists in the selected word
     for position in range(len(selected_word)):
         letter = selected_word[position]
-        if letter == user:
-            blanks[position] = letter
-    print(blanks)
+        if letter == user_guess:
+            user_display[position] = letter
+    user_display_string = ' '.join(user_display)
+    print(user_display_string)
+
+    # Remove 1 chance from the user chances, when it's 0 stop iteration.
+    if user_guess not in selected_word:
+        chances -= 1
+        if chances == 0:
+            is_game_over = True
+            print(f'The correct word is {selected_word}. You Lose, Try Again!!!')
+        # display the user chances stage
+        print(chances_stage[chances])
 
     # Stop iteration when there are no more blanks
-    if '_' not in blanks:
+    if '_' not in user_display:
         is_game_over = True
-        print(f'The word is {selected_word}, You Win')
+        print(f'You guessed the word "{selected_word}" correctly. You Win!!!')
+
+        if chances > 1:
+            print(f'You finished with {chances} chances remaining.')
+            print(chances_stage[chances])
+        else:
+            print(f'You finished with {chances} chance remaining, wow that was close.')
+            print(chances_stage[chances])
