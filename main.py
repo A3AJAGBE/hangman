90a1fb081daa0fc4a7eda5f141a0b93c5a038837
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-24 06:01:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+This application is a basic hangman game.
+A random word is chosen from a list and length of the word is showed in blanks to the user.
+A user start's the game with 4 chances to guess the word correctly by guessing the letters in the word.
+If the user guessed correctly before the chances is at 0 they win else they lose,
+while each wrong guess means the user loses a chance.
+"""
